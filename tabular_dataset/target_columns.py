43f9a051f3c74d92128245093bf2055c03300d11
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-06-23 20:32:58 +0200

    Add target.encode()

--- tabular_dataset/target_columns.py
+++ tabular_dataset/target_columns.py
@@ -2,3 +2,8 @@ class TargetColumns:
   def __init__(self, ds, column_name):
     self.ds = ds
     self.column_names = [column_name]
+
+  def encode(self, one_hot: bool = False):
+    encode_categorical_columns(self, one_hot)
+
+    return self.ds  # For fluent API
--- tests/test_target_columns.py
+++ tests/test_target_columns.py
@@ -16,11 +16,32 @@ def get_test_df():
 
 
 def test_column_names_are_correctly_set():
-    df = get_test_df()
+  df = get_test_df()
 
-    tds = TabularDataset(df, target_column='target')
+  tds = TabularDataset(df, target_column='target')
 
-    assert tds.target.column_names == ['target']
+  assert tds.target.column_names == ['target']
+
+
+def test_encode():
+  df = get_test_df()
+
+  tds = TabularDataset(df, target_column='target')
+  tds.target.encode()
+
+  assert repr(tds.y) == repr(np.array([0, 1, 2, 0]).reshape(-1, 1))
+
+
+def test_encode_one_hot():
+  df = get_test_df()
+
+  tds = TabularDataset(df, target_column='target')
+  tds.target.encode(one_hot=True)
+
+  assert repr(tds.y) == repr(np.array([[1., 0., 0.],
+                                       [0., 1., 0.],
+                                       [0., 0., 1.],
+                                       [1., 0., 0.]]))
 
 
 if __name__ == '__main__':
