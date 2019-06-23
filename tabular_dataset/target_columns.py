9c3bb5703ed0721478fe6fde1dc34a950761850f
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-06-23 20:45:21 +0200

    Fix bug and remove commented-out code

--- tabular_dataset/target_columns.py
+++ tabular_dataset/target_columns.py
@@ -1,7 +1,12 @@
+from tabular_dataset.util import encode_categorical_columns
+
+
 class TargetColumns:
   def __init__(self, ds, column_name):
     self.ds = ds
     self.column_names = [column_name]
+
+    self._encoders = {}
 
   def encode(self, one_hot: bool = False):
     encode_categorical_columns(self, one_hot)
--- tabular_dataset/util.py
+++ tabular_dataset/util.py
@@ -4,35 +4,6 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-# class MultiColumnLabelEncoder:
-#   """
-#
-#   Code mostly taken from https://stackoverflow.com/a/30267328
-#   """
-#   def __init__(self, columns = None):
-#     self.columns = columns # array of column names to encode
-#
-#   def fit(self, X, y=None):
-#     return self # not relevant here
-#
-#   def transform(self, X):
-#     """Transforms columns of X specified in self.columns using LabelEncoder().
-#
-#     If no columns specified, transforms all columns in X.
-#     """
-#     output = X.copy()
-#     if self.columns is not None:
-#       for col in self.columns:
-#         output[col] = LabelEncoder().fit_transform(output[col])
-#     else:
-#       for colname, col in output.iteritems():
-#         output[colname] = LabelEncoder().fit_transform(col)
-#     return output
-#
-#   def fit_transform(self, X, y=None):
-#     return self.fit(X, y).transform(X)
-
-
 def encode_categorical_columns(columns, one_hot: bool = False):
   all_new_column_names = []
   for column_name in columns.column_names:
@@ -43,7 +14,7 @@ def encode_categorical_columns(columns, one_hot: bool = False):
         columns.ds.df[columns.column_names].values.reshape(-1, 1))
       # TODO Drop the first column per feature
       new_column_names = (columns._encoders[column_name]
-                          .get_feature_names(column_name))
+                          .get_feature_names([column_name]))
       columns.ds.df = columns.ds.df.drop(column_name, axis=1)
       for i, new_column in enumerate(new_column_names):
         columns.ds.df[new_column] = encoded_values[:, i]
