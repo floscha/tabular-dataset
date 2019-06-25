eb40044d76e8c407baa28356fb0ca4c5c411245f
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-06-25 17:09:21 +0200

    Fix column assignment

--- tabular_dataset/util.py
+++ tabular_dataset/util.py
@@ -9,7 +9,7 @@ def encode_categorical_columns(columns_obj, columns):
     encoder = LabelEncoder()
     # FIXME Only convert columns with object type to str?
     columns_obj.ds.df[column_name] = encoder.fit_transform(
-      columns_obj.ds.df[columns_obj.column_names].values.astype(str).ravel())
+      columns_obj.ds.df[column_name].values.astype(str).ravel())
     columns_obj._encoders[column_name] = encoder
 
 
@@ -19,14 +19,16 @@ def hash_categorical_columns(columns_obj, columns, bins: int):
 
 
 def one_hot_categorical_columns(columns_obj, columns):
-  old_column_names = columns_obj.column_names
+  old_column_names = columns
   ohe = OneHotEncoder(categories='auto', sparse=False)
-  new_column_names = list(ohe.fit(columns_obj.ds.df[old_column_names])
-                          .get_feature_names(old_column_names))
+  ohe.fit(columns_obj.ds.df[old_column_names])
+  new_column_names = list(ohe.get_feature_names(old_column_names))
   columns_obj.ds.df = columns_obj.ds.df.join(
     pd.DataFrame(ohe.transform(columns_obj.ds.df[old_column_names]),
                  columns=new_column_names)
   )
   # TODO Drop the first column per feature
   columns_obj.ds.df = columns_obj.ds.df.drop(old_column_names, axis=1)
-  columns_obj.column_names = new_column_names
+  columns_obj.column_names = ([cn for cn in columns_obj.column_names
+                               if cn not in old_column_names]
+                              + new_column_names)
