d08fb297817f526a6831257edeeaf0e4eb4d2291
Florian Schäfer <33159547+floscha@users.noreply.github.com>
2019-09-25 23:14:38 +0200

    Add impute method for datetime columns (#67, resolves #65)
    * Add impute method for datetime columns
    
    * Remove empty line causing linter error

--- tests/test_datetime_columns.py
+++ tests/test_datetime_columns.py
@@ -16,6 +16,10 @@ def get_test_df():
     })
 
 
+def get_df_with_missing_values():
+    return pd.DataFrame({'dt': ['1/1/2018', '1/1/2018', '1/2/2018', None]})
+
+
 def test_column_names_are_correctly_set():
     df = get_test_df()
 
@@ -24,7 +28,42 @@ def test_column_names_are_correctly_set():
     assert tds.datetime.column_names == ['dt1', 'dt2']
 
 
-def test_encode_defaults_are_correct2():
+def test_impute():
+    df = get_df_with_missing_values()
+    tds = TabularDataset(df, datetime_columns=['dt'])
+
+    tds.datetime.impute()
+
+    assert tds.x[-1] == ['1/1/2018']
+
+
+def test_impute_value_is_correctly_encoded():
+    # This test assumes that the `encode()` method is correctly implemented.
+    df = get_df_with_missing_values()
+    tds = TabularDataset(df, datetime_columns=['dt'])
+
+    tds.datetime.impute()
+    tds.datetime.encode()
+
+    assert repr(tds.x[-1]) == repr(np.array([2018, 1, 1, 1, 0, 0, 0, 0]))
+
+
+def test_impute_with_test_data():
+    # Assert that the median from the test set is not leaked into the train set
+    # but instead the train set median is used.
+    df = get_df_with_missing_values()
+    train_data = df.iloc[[2]]
+    test_data = df.iloc[[0, 1, 3]]
+    tds = TabularDataset(train_data, test_data=test_data,
+                         datetime_columns=['dt'])
+
+    tds.datetime.impute()
+
+    _ = tds.x_train
+    assert tds.x_test[-1][0] == '1/2/2018'
+
+
+def test_encode_defaults_are_correct():
     df = get_test_df()
 
     tds = TabularDataset(df, datetime_columns=['dt1', 'dt2'])
